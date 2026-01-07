f3a41d0d00e2d5d027b5e78483dc5812773e1c0f
meniyarohit8610-coder <meniyarohit8610@gmail.com>
2026-01-07 15:18:25 +0530

    Add files via upload

---
+++ python/10_list_2.py
@@ -0,0 +1,26 @@
+###################### destination ######################
+destination=["kaccha","goa","gir","jamm and kashmir","himanchasl pradesh"]
+
+print(destination)
+
+#for print gir(2) 
+print(destination[2])
+
+#for print goa to himanchal pradesh 
+print(destination[2:])
+
+###################### create a list & store 10 different type of values in it ######################
+values=[101,"meniya rohit","bca",5,3,2006,"vallbhipur","bhavnagar","gujrat","india"]
+#print all values 
+print(values)
+
+#print 1st value 
+print(values[0])
+#print 1st five value 
+print(values[0:5])
+#print last 5 value 
+print(values[5:11])
+#print list twice using * 
+print(values*2)
+#print item in the list from 2nd to 5th position
+print(values[1:6])
---
+++ python/9_tupple_2.py
@@ -0,0 +1,7 @@
+states = ("Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal")
+
+print(states)
+print("Position of goa ",states.index('Goa'))
+# print("Position of gujarat ",states.index('Delhi')) #value not found is error
+print("Count of Maharashtra ",states.count('Maharashtra')) #1
+print("Count of diu and daman ",states.count('div and daman')) #0
