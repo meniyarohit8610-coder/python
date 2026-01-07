b161f9bd997fe35ac79400f4cb9751618e01fe5d
meniyarohit8610-coder <meniyarohit8610@gmail.com>
2026-01-07 15:37:34 +0530

    Add files via upload

---
+++ python/11_tupple_3..py
@@ -0,0 +1,18 @@
+states = ("Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal")
+
+#display tuple 
+print(states)
+#display 1st five items
+print(states[1:6]) 
+#display 2nd and 3rd and 4th item 
+print(states[2:5])
+#display all items from 7th position onwards 
+print(states[6:])
+#try to remove 3rd item see what happens 
+  
+### it generate error ###
+
+'''
+states.remove[3]
+print(states)
+'''
