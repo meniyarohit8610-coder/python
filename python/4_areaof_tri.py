6e7b9df376322d90031a04e5e1864f5888c0832d
meniyarohit8610-coder <meniyarohit8610@gmail.com>
2026-01-22 13:03:33 +0530

    commit

--- python/4_areaof_tri.py
+++ python/4_areaof_tri.py
@@ -3,14 +3,14 @@ are of triangle
 0.5*base*hight
 '''
 base=0
-hight=0
+height=0
 
 base=input("enter value of base ..")
-hight=input("enter value of hight ")
+height=input("enter value of hight ")
 
 base=int(base)
-hight=int(hight)
+height=int(height)
 
-ans=0.5*base*hight
+ans=0.5*base*height
 
 print("value of triangle is : ",ans)
