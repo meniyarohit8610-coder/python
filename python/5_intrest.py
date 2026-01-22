c2bc1f7a62f5ac9548075ab5c614f9448f573439
meniyarohit8610-coder <meniyarohit8610@gmail.com>
2026-01-22 13:10:44 +0530

    main

--- python/5_intrest.py
+++ python/5_intrest.py
@@ -6,7 +6,7 @@ r=0
 n=0
 
 p=input("enter principal (ammount): ")
-r=input("enter rate of intrest : ")
+r=input("enter rate of interest : ")
 n=input("enter number of year : ")
 
 p=float(p)
@@ -15,4 +15,4 @@ n=float(n)
 
 ans=p*r*n/100
 
-print("intrest is ",ans)
+print("interest is ",ans)
