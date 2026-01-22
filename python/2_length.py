9cd2c751b836dc84a787095d7a130f1290a179c2
meniyarohit8610-coder <meniyarohit8610@gmail.com>
2026-01-22 12:58:37 +0530

    changed name and save

---
+++ python/2_length.py
@@ -0,0 +1,16 @@
+'''
+programme for ractangle
+'''
+length=0
+width =0
+
+length=input("enter value for length...")
+width=input("enter value for width...")
+
+length=int(length)
+width =int(width)
+
+radious=length*width
+
+print("total redious is : ",radious)
+
