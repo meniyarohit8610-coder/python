631b415d96783b89498e9b7a30cd0b71bcd31b6f
meniyarohit8610-coder <meniyarohit8610@gmail.com>
2026-01-27 16:01:57 +0530

    commit

--- python/46_opration_elif.py
+++ python/46_opration_elif.py
@@ -16,8 +16,9 @@ elif choice==2:
 elif choice==3:
     multiplication=num_1*num_2
     print("multiplication is : ",multiplication)
-else:
+elif choice==4:
     division = num_1 / num_2
     print("divisison is :",division )
-
+else:
+    print("enter proper choise !!!!! ")
 print(":) :) good byyy :) :) ")
