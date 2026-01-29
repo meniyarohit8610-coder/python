e146f190647c31ec025ba2577c3833b8d5843018
meniyarohit8610-coder <meniyarohit8610@gmail.com>
2026-01-29 14:52:43 +0530

    main

--- python/48_zodiac_elif.py
+++ python/48_zodiac_elif.py
@@ -3,7 +3,7 @@
    
     Aquarius: January 20-February 18
     Pisces: February 19-March 20
-     Aries: March 21-April 19
+    Aries: March 21-April 19
     Taurus: April 20-May 20
     Gemini: May 21-June 21
     Cancer: June 22-July 22
@@ -14,8 +14,9 @@
     Sagittarius: November 22-December 21
     Capricorn: December 22-January 19
     '''
-date=int(input("enter date of birth : "))
-month=int(input("enter birth month : "))
+
+date=int(input("enter date of birth of male : "))
+month=int(input("enter birth month of male : "))
 
 if month==1 and date>=20 or month==2 and date<=18:
     print("Aquarius")
@@ -41,3 +42,32 @@ elif month==11 and date>=22 or month==12 and date<=21:
     print("Sagittarius")
 else:
     print("Capricorn")
+
+date1=int(input("enter date of birth of female : "))
+month1=int(input("enter birth month of female : "))
+
+if month1==1 and date1>=20 or month1==2 and date1<=18:
+    print("Aquarius")
+elif month1==2 and date1>=19 or month1==3 and date1<=20:
+    print("Pisces")
+elif month1==3 and date1>=21 or month1==4 and date1<=19:
+    print("Aries")
+elif month1==4 and date1>=20 or month1==5 and date1<=20:
+    print("Taurus")
+elif month1==5 and date1>=21 or month1==6 and date1<=21:
+    print("Gemini")
+elif month1==6 and date1>=22 or month1==7 and date1<=22:
+    print("Cancer")
+elif month1==7 and date1>=23 or month1==8 and date1<=22:
+    print("Leo")
+elif month1==8 and date1>=23 or month1==9 and date1<=22:
+    print("Virgo")
+elif month1==9 and date1>=23 or month1==10 and date1<=22:
+    print("Libra")
+elif month1==10 and date1>=24 or month1==11 and date1<=21:
+    print("Scorpio")
+elif month1==11 and date1>=22 or month1==12 and date1<=21:
+    print("Sagittarius")
+else:
+    print("Capricorn")
+
