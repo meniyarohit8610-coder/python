bec327485b9c525576519950164bc8dbc6726d0d
meniyarohit8610-coder <meniyarohit8610@gmail.com>
2026-01-29 15:19:41 +0530

    main

--- python/48_zodiac_elif.py
+++ python/48_zodiac_elif.py
@@ -15,8 +15,8 @@
     Capricorn: December 22-January 19
     '''
 
-date=int(input("enter date of birth of male : "))
-month=int(input("enter birth month of male : "))
+date=int(input("enter date of birth  : "))
+month=int(input("enter birth month   : "))
 
 if month==1 and date>=20 or month==2 and date<=18:
     print("Aquarius")
@@ -42,32 +42,3 @@ elif month==11 and date>=22 or month==12 and date<=21:
     print("Sagittarius")
 else:
     print("Capricorn")
-
-date1=int(input("enter date of birth of female : "))
-month1=int(input("enter birth month of female : "))
-
-if month1==1 and date1>=20 or month1==2 and date1<=18:
-    print("Aquarius")
-elif month1==2 and date1>=19 or month1==3 and date1<=20:
-    print("Pisces")
-elif month1==3 and date1>=21 or month1==4 and date1<=19:
-    print("Aries")
-elif month1==4 and date1>=20 or month1==5 and date1<=20:
-    print("Taurus")
-elif month1==5 and date1>=21 or month1==6 and date1<=21:
-    print("Gemini")
-elif month1==6 and date1>=22 or month1==7 and date1<=22:
-    print("Cancer")
-elif month1==7 and date1>=23 or month1==8 and date1<=22:
-    print("Leo")
-elif month1==8 and date1>=23 or month1==9 and date1<=22:
-    print("Virgo")
-elif month1==9 and date1>=23 or month1==10 and date1<=22:
-    print("Libra")
-elif month1==10 and date1>=24 or month1==11 and date1<=21:
-    print("Scorpio")
-elif month1==11 and date1>=22 or month1==12 and date1<=21:
-    print("Sagittarius")
-else:
-    print("Capricorn")
-
