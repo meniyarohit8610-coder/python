1a26e5f6c019788894714e2f1637485773d7deec
meniyarohit8610-coder <meniyarohit8610@gmail.com>
2026-02-02 14:52:17 +0530

    main

---
+++ python/68_armstrong_loop.py
@@ -0,0 +1,8 @@
+#write a program to figure out whether given number  is armstrong number or not
+
+number=int(input("enter any number : "))
+count=0
+
+while count<number:
+    count=count+1
+    print(count)
