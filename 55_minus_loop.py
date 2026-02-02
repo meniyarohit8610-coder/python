a2e5423d8f2ef0c06cde89e366f4b6f7917a728e
meniyarohit8610-coder <meniyarohit8610@gmail.com>
2026-02-02 10:56:34 +0530

    Delete 55_minus_loop.py

--- 55_minus_loop.py
+++
@@ -1,8 +0,0 @@
-# 100  95  90  85  80  75  70  65  60  ..... 0
-
-number=100
-
-print(number,end=' ') #for print first number 100
-while number>0:
-    number=number-5
-    print(number,end=' ') #for print value number minus 5 till 0.
