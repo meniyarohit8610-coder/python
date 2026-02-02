7c8ad3ab3ae377783612aac1ae4618411e4f2f91
meniyarohit8610-coder <meniyarohit8610@gmail.com>
2026-02-02 14:21:43 +0530

    main

--- python/67_perfect_while.py
+++ python/67_perfect_while.py
@@ -1,4 +1,15 @@
-#write a program to figure out whether given number  is perfect number or not
-import sys
-number=int(input("enter the number : "))
 
+#write a program to figure out whether given number  is perfect num
+
+num=int(input("Enter a number: "))
+sum=0
+i=1
+while i<num:
+    if num%i==0:
+        sum=sum+i
+    i=i+1   
+if sum==num:
+    print(f"{num} is a perfect number") 
+else:
+    print(f"{num} is not a perfect number")
+# A perfect number is a positive integer that is equal to the sum of its proper positive divisors, excluding the number itself.
